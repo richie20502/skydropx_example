c21ee88faef944155f0f4365b3a1457c54fb9fbc
richie20502 <richie.lugo.recillas.1990@gmail.com>
2024-12-13 23:31:53 -0600

    colection

--- resources/views/shipments/create.blade.php
+++ resources/views/shipments/create.blade.php
@@ -19,42 +19,42 @@
     <form action="{{ route('shipments.store') }}" method="POST">
         @csrf
         <!-- Campos ocultos con valores predefinidos -->
-        <input type="hidden" name="origin_name" value="Juan Pérez">
-        <input type="hidden" name="origin_company" value="Empresa Origen SA de CV">
-        <input type="hidden" name="origin_email" value="juan.perez@empresa-origen.com">
-        <input type="hidden" name="origin_phone" value="5551234567">
-        <input type="hidden" name="origin_street" value="Av. Insurgentes Sur">
-        <input type="hidden" name="origin_number" value="1602">
-        <input type="hidden" name="origin_district" value="Crédito Constructor">
-        <input type="hidden" name="origin_city" value="Ciudad de México">
-        <input type="hidden" name="origin_state" value="CM">
-        <input type="hidden" name="origin_country" value="MX">
-        <input type="hidden" name="origin_postal_code" value="03940">
+        <input type="text" name="origin_name" value="Juan Pérez">
+        <input type="text" name="origin_company" value="Empresa Origen SA de CV">
+        <input type="text" name="origin_email" value="juan.perez@empresa-origen.com">
+        <input type="text" name="origin_phone" value="5551234567">
+        <input type="text" name="origin_street" value="Av. Insurgentes Sur">
+        <input type="text" name="origin_number" value="1602">
+        <input type="text" name="origin_district" value="Crédito Constructor">
+        <input type="text" name="origin_city" value="Ciudad de México">
+        <input type="text" name="origin_state" value="CM">
+        <input type="text" name="origin_country" value="MX">
+        <input type="text" name="origin_postal_code" value="03940">
 
-        <input type="hidden" name="destination_name" value="María Rodríguez">
-        <input type="hidden" name="destination_company" value="Empresa Destino SA de CV">
-        <input type="hidden" name="destination_email" value="maria.rodriguez@empresa-destino.com">
-        <input type="hidden" name="destination_phone" value="8183456789">
-        <input type="hidden" name="destination_street" value="Av. Lázaro Cárdenas">
-        <input type="hidden" name="destination_number" value="2400">
-        <input type="hidden" name="destination_district" value="Residencial San Agustín">
-        <input type="hidden" name="destination_city" value="San Pedro Garza García">
-        <input type="hidden" name="destination_state" value="NL">
-        <input type="hidden" name="destination_country" value="MX">
-        <input type="hidden" name="destination_postal_code" value="66260">
+        <input type="text" name="destination_name" value="María Rodríguez">
+        <input type="text" name="destination_company" value="Empresa Destino SA de CV">
+        <input type="text" name="destination_email" value="maria.rodriguez@empresa-destino.com">
+        <input type="text" name="destination_phone" value="8183456789">
+        <input type="text" name="destination_street" value="Av. Lázaro Cárdenas">
+        <input type="text" name="destination_number" value="2400">
+        <input type="text" name="destination_district" value="Residencial San Agustín">
+        <input type="text" name="destination_city" value="San Pedro Garza García">
+        <input type="text" name="destination_state" value="NL">
+        <input type="text" name="destination_country" value="MX">
+        <input type="text" name="destination_postal_code" value="66260">
 
-        <input type="hidden" name="package_content" value="Documentos">
-        <input type="hidden" name="package_amount" value="1">
-        <input type="hidden" name="package_type" value="envelope">
-        <input type="hidden" name="package_weight" value="0.5">
-        <input type="hidden" name="package_insurance" value="0">
-        <input type="hidden" name="package_declared_value" value="100">
-        <input type="hidden" name="package_length" value="30">
-        <input type="hidden" name="package_width" value="20">
-        <input type="hidden" name="package_height" value="2">
+        <input type="text" name="package_content" value="Documentos">
+        <input type="text" name="package_amount" value="1">
+        <input type="text" name="package_type" value="envelope">
+        <input type="text" name="package_weight" value="0.5">
+        <input type="text" name="package_insurance" value="0">
+        <input type="text" name="package_declared_value" value="100">
+        <input type="text" name="package_length" value="30">
+        <input type="text" name="package_width" value="20">
+        <input type="text" name="package_height" value="2">
 
-        <input type="hidden" name="carrier" value="fedex">
-        <input type="hidden" name="service" value="express">
+        <input type="text" name="carrier" value="fedex">
+        <input type="text" name="service" value="express">
 
         <button type="submit" class="btn btn-primary">Crear Envío con Valores Predefinidos</button>
     </form>
